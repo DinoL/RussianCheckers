6a7eb608deb4149f368e4919e411eac506e1b643
DinoL <grech.leonid@gmail.com>
2023-06-21 00:19:05 +0300

    [+] Add test for free moves

--- test/test_direction.cpp
+++ test/test_direction.cpp
@@ -13,7 +13,12 @@ TEST(DirectionTests, AllMoves)
     EXPECT_EQ(Direction::bottom_left().all_moves(s), 0x04663315);
 }
 
-TEST(DirectionTests, TestFail)
+TEST(DirectionTests, FreeMoves)
 {
-    EXPECT_TRUE(false);
+    state_t s = 0x00400400;
+    state_t p = 0x00188208;
+    EXPECT_EQ(Direction::top_right().free_moves(s, p), 0x88004000);
+    EXPECT_EQ(Direction::top_left().free_moves(s, p), 0x24022000);
+    EXPECT_EQ(Direction::bottom_right().free_moves(s, p), 0x40);
+    EXPECT_EQ(Direction::bottom_left().free_moves(s, p), 0x00042022);
 }
