bd2625a5fd0bd15e63f5ba8cab929af9182ace8d
DinoL <grech.leonid@gmail.com>
2023-07-04 22:39:25 +0300

    [+] Add some connection logic

--- src/network_connection.cpp
+++ src/network_connection.cpp
@@ -1,7 +1,14 @@
 #include "network_connection.h"
 
+#include <QNetworkInterface>
+
 NetworkConnection::NetworkConnection()
-{}
+{
+    connect(&_listening_socket,
+            SIGNAL(newConnection()),
+            this,
+            SLOT(slot_accept_connection()));
+}
 
 bool NetworkConnection::is_connected_remotely() const
 {
@@ -18,7 +25,25 @@ bool NetworkConnection::is_ready() const
     return false;
 }
 
-QHostAddress NetworkConnection::preferred_host_address(const QAbstractSocket::NetworkLayerProtocol protocol)
+QHostAddress NetworkConnection::preferred_host_address(
+    const QAbstractSocket::NetworkLayerProtocol protocol)
 {
-    return QHostAddress();
+    for (const auto& interface : QNetworkInterface::allInterfaces())
+    {
+        const auto flags = interface.flags();
+        if(!(flags & QNetworkInterface::IsLoopBack)
+            && (flags & QNetworkInterface::IsUp))
+        {
+            for (const auto& address : interface.addressEntries())
+            {
+                if(address.ip().protocol() == protocol)
+                    return address.ip();
+            }
+        }
+    }
+
+    if(protocol == QAbstractSocket::IPv4Protocol)
+        return QHostAddress(QHostAddress::LocalHost);
+    else
+        return QHostAddress(QHostAddress::LocalHostIPv6);
 }
